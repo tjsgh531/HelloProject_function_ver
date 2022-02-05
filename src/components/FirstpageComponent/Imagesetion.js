aa327aa67a5f3fcabe0f588c6c840270b4906fb9
Sun Ho <tjsgh5312@daum.net>
2022-02-05 11:15:56 +0900

    check point

--- src/components/FirstpageComponent/Imagesetion.js
+++ src/components/FirstpageComponent/Imagesetion.js
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 import img1 from '../../image/1.jpg';
 import img2 from '../../image/2.jpg';
@@ -17,22 +17,31 @@ const Styledimagesection = styled.div`
 const Imgitem = styled.img`
     width:100%;
     height:100%;
-
-    display:${(props) => props.ishidden ? none : auto}
 `;
 
 function Imagesetion(){
+    const img_maxsize = 4;
+    let [_selected_id, setSelectedId] = useState(1);
+    
+    useEffect(function(){
+        const imgchangeInterval = setInterval(()=>{
+            let temp = (++_selected_id % 4) +1;
+            console.log(temp);
+            setSelectedId(temp);
+        },5000);     
+        return(function(){
+           clearInterval(imgchangeInterval);
+        })
+    },[]);
 
     return(
         <Styledimagesection>
-            <Imgitem src={img1}></Imgitem>
+            { _selected_id == 1 ? <Imgitem src={img1}></Imgitem> : null}
+            { _selected_id == 2 ? <Imgitem src={img2}></Imgitem> : null}
+            { _selected_id == 3 ? <Imgitem src={img3}></Imgitem> : null}
+            { _selected_id == 4 ? <Imgitem src={img4}></Imgitem> : null}  
         </Styledimagesection>
     );
-}
-
-function ImageItem(){
-    let items =[];
-
 }
 
 export default Imagesetion;
